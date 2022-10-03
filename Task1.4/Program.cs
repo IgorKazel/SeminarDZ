189dc848830899540d94551ab5352db2d1b33566
Igor Kazel <igor.joer@gmail.com>
2022-10-03 23:02:14 +0300

    Task1.4 added

---
+++ Task1.4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+if (count <= N)
+{
+    Console.WriteLine($"Все четные числа от 1 до {N}:");
+}
+else
+{
+    Console.WriteLine("Введенное число меньше или равно 1");
+}
+while (count <= N)
+{
+    Console.Write($"{count} ");
+    count = count + 2;
+}
