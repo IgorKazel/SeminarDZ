9e59f3fa7c94668884514686443d89cdb10e6586
Igor Kazel <igor.joer@gmail.com>
2022-10-03 22:43:44 +0300

    Task1.3 added

---
+++ Task1.3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int ost = a % 2;
+if (ost == 0)
+{
+    Console.WriteLine($"Число {a} - четное");
+}
+else
+{
+    Console.WriteLine($"Число {a} - нечетное");
+}
