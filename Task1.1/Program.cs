21e3c41c91ddba5937be93335bce4cb46feee44b
Igor Kazel <igor.joer@gmail.com>
2022-10-03 22:26:08 +0300

    Task1.1 added

---
+++ Task1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine($"Максимальное число - {a}, минимальное число - {b}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число - {b}, минимальное число - {a}");
+}
