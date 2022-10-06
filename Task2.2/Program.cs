750c51581b33c683fe7fb9b75d59a854083b37b1
Igor Kazel <igor.joer@gmail.com>
2022-10-06 16:42:48 +0300

    Task2.2 added

---
+++ Task2.2/Program.cs
@@ -0,0 +1,6 @@
+﻿int a = new Random().Next(100,1000);
+Console.WriteLine($"Случайное трехзначное число - {a}");
+int a1 = a / 100;
+int a3 = a % 10;
+int a13 = a1 * 10 + a3;
+Console.WriteLine($"Данное число без второй цифры - {a13}");
