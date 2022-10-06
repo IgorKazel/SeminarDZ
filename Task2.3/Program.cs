e6892b41966d3f7190fdad2325f531b541f2a2ae
Igor Kazel <igor.joer@gmail.com>
2022-10-06 16:54:35 +0300

    Task2.3 added

---
+++ Task2.3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int a3 = a;
+if (a > 99)
+{
+    while (a3 > 999)
+    {
+        a3 = a3 / 10;
+    }
+    a3 = a3 % 10;
+    Console.WriteLine($"Третья цифра числа {a} - {a3}");
+}
+else
+{
+    Console.WriteLine($"У числа {a} - нет третьей цифры");
+}
