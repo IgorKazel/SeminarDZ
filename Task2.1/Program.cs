9326e70d0daf7e6a58b45c524d59830ba83fa337
Igor Kazel <igor.joer@gmail.com>
2022-10-06 16:22:43 +0300

    Task2.1 added

---
+++ Task2.1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 99 & a < 1000)
+{
+    int number2 = a / 10 % 10;
+    Console.WriteLine($"Вторая цифра числа {a} - {number2}");
+}
+else
+{
+    Console.WriteLine($"Число {a} - не трехзначное");
+}
