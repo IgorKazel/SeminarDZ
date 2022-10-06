12e496b1582aa03a3e46bb11392a09c54ae1e59a
Igor Kazel <igor.joer@gmail.com>
2022-10-06 17:04:37 +0300

    Task2.4 added

---
+++ Task2.4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 8)
+{
+    if (a > 5)
+    {
+        Console.WriteLine($"{a} день недели - выходной");
+    }
+    else
+    {
+        Console.WriteLine($"{a} день недели - будний");
+    }
+}
+else
+{
+    Console.WriteLine($"Число {a} не обозначает день недели. В неделе 7 дней.");
+}
