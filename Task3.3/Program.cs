54fc7b9c3426287ba6c5f4d2b1772ac0d9b0e4e6
Igor Kazel <igor.joer@gmail.com>
2022-10-09 22:00:09 +0300

    Task3.3 added

---
+++ Task3.3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N>=1)
+{
+    Console.WriteLine($"Кубы чисел от 1 до {N}:");
+    for (int i = 1; i <= N; i++)
+    {
+        Console.WriteLine(Math.Pow(i, 3));
+    }
+}
+else
+{
+    Console.WriteLine("Введенное число меньше 1");
+}
