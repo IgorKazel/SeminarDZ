e4cd044ad4dbd8dfffa43f7de3993e7a71279aa0
Igor Kazel <igor.joer@gmail.com>
2022-10-09 21:01:24 +0300

    Task3.1 added

---
+++ Task3.1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 9999 & a < 100000)
+{
+    if (a / 10000 == a % 10 & a / 1000 % 10 == a / 10 % 10)     // Сравниванем 1-ую цифру с 5-ой и 2-ую с 4-ой
+    {
+        Console.WriteLine($"Число {a} - палендром");
+    }
+    else
+    {
+        Console.WriteLine($"Число {a} - не палендром");
+    }  
+}
+else
+{
+    Console.WriteLine($"Число {a} - не пятизначное");
+}
