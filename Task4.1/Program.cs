06967241b07763d04957645631f4eb7ab7f65a4b
Igor Kazel <igor.joer@gmail.com>
2022-10-12 18:42:05 +0300

    Task4.1 added

---
+++ Task4.1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число A");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число B");
+int B = Convert.ToInt32(Console.ReadLine());
+if (B >= 0)
+{
+    int result = 1;
+    for (int i = 0; i < B; i++)
+    {
+        result = result * A;
+    }
+    Console.WriteLine($"Число {A} в степени {B} равно {result}");
+}
+else
+{
+    Console.WriteLine($"Число {B} не натуральное");
+}
