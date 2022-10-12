e45be0b62998bffd7f646803e24628f8c1667129
Igor Kazel <igor.joer@gmail.com>
2022-10-12 20:25:40 +0300

    Task4.Star added

---
+++ Task4.Star/Program.cs
@@ -0,0 +1,44 @@
+﻿int[] CreateArray()
+{
+    int[] array = new int[10];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0,10);
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+    return array;
+}
+
+int SearchSecondMax (int[] array)
+{
+    int max = array[0];
+    int secondMax = 0;
+    if (max > array[1])
+    {
+        secondMax = array[1];
+    }
+    else
+    {
+        secondMax = max;
+        max = array[1];
+    }
+    for (int i = 2; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            secondMax = max;
+            max = array[i];
+        }
+        else if (array[i] > secondMax & array[i] != max)
+        {
+            secondMax = array[i];
+        }
+    }
+    return secondMax;
+}
+
+Console.WriteLine("Случайный массив из 10 чисел:");
+int[] arr = CreateArray();
+int secMax = SearchSecondMax(arr);
+Console.WriteLine($"Второй максимум из данного массива {secMax}");
