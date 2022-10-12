36fdd5872553f5b7982cc24a09e58f95ff33da83
Igor Kazel <igor.joer@gmail.com>
2022-10-12 19:12:46 +0300

    Task4.2 added

---
+++ Task4.2/Program.cs
@@ -0,0 +1,19 @@
+﻿int SumDigits(int number)
+{
+    if (number < 0)
+    {
+        number = -number;
+    }
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int sumDig = SumDigits(a);
+Console.WriteLine($"Сумма цифр числа {a} равна {sumDig}");
