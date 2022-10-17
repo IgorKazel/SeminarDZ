3bd7240f5375cdd4d262293f90235c2a38c0ab72
Igor Kazel <igor.joer@gmail.com>
2022-10-17 20:59:28 +0300

    Task5.2 added

---
+++ Task5.2/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] CreateArray(int length, int minLimit, int maxLimit)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minLimit, maxLimit + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array, string message)
+{
+    Console.WriteLine(message);
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int SumUnevenPosition(int[] array)
+{
+    int Sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+       Sum += array[i];
+    }
+    return Sum;
+}
+
+int[] arr = CreateArray(4, -99, 99);
+PrintArray(arr, "Массив случайных чисел:");
+int sum = SumUnevenPosition(arr);
+Console.WriteLine($"Сумма чисел, стоящих на нечётных позициях - {sum}");
