ffcd722a4e0b19b55df4600d1a27a4adb95aeae4
Igor Kazel <igor.joer@gmail.com>
2022-10-24 20:40:59 +0300

    Entering size from keyboard added

--- Task7.1/Program.cs
+++ Task7.1/Program.cs
@@ -1,11 +1,18 @@
-﻿double[,] FillArray(int line, int column)
+﻿int NumberEntry(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+double[,] FillArray(int line, int column)
 {
     double[,] array = new double[line, column];
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = new Random().Next(-9, 10) + new Random().Next(-9, 10) / 10.0;
+            array[i, j] = new Random().Next(-99, 100) / 10.0;
         }
     }
     return array;
@@ -24,5 +31,12 @@ void PrintArray(double[,] array, string message)
     }
 }
 
-double[,] arr = FillArray(3, 4);
+int m = NumberEntry("Задайте количество строк в массиве");
+int n = NumberEntry("Задайте количество столбцов в массиве");
+if (m < 1 || n < 1)
+{
+    Console.WriteLine("Количество строк и столбцов в массиве не может быть меньше 1");
+    return;
+}
+double[,] arr = FillArray(m, n);
 PrintArray(arr, "Случайный массив вещественных чисел:");
