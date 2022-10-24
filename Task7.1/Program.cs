4c385bf6d67a832a58b13cb5bb0287d1fbd31782
Igor Kazel <igor.joer@gmail.com>
2022-10-24 19:33:58 +0300

    Code formatting

--- Task7.1/Program.cs
+++ Task7.1/Program.cs
@@ -1,11 +1,11 @@
 ﻿double[,] FillArray(int line, int column)
 {
-    double[,] array = new double[line,column];
+    double[,] array = new double[line, column];
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i,j] = new Random().Next(-9,10) + new Random().Next(-9,10)/10.0;  
+            array[i, j] = new Random().Next(-9, 10) + new Random().Next(-9, 10) / 10.0;
         }
     }
     return array;
@@ -18,11 +18,11 @@ void PrintArray(double[,] array, string message)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write($"{array[i,j]:f1}\t");  
+            Console.Write($"{array[i, j]:f1}\t");
         }
         Console.WriteLine();
     }
 }
 
-double[,] arr = FillArray(3,4);
+double[,] arr = FillArray(3, 4);
 PrintArray(arr, "Случайный массив вещественных чисел:");
