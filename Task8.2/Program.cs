3474531a08c66c3f30a6ea121bf22c1881f4ae26
Igor Kazel <igor.joer@gmail.com>
2022-11-10 20:13:35 +0300

    Task8.2 added

---
+++ Task8.2/Program.cs
@@ -0,0 +1,59 @@
+﻿int[,] FillArray(int line, int column)
+{
+    int[,] array = new int[line, column];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array, string message)
+{
+    Console.WriteLine(message);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] SumEachLine(int[,] array)
+{
+    int[] result = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        result[i] = sum;
+    }
+    return result;
+}
+
+int MinNumberIndex(int[] array)
+{
+    int indexMin = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[indexMin] > array[i])
+        {
+            indexMin = i;
+        }
+    }
+    return indexMin + 1;
+}
+
+int[,] arr = FillArray(4, 3);
+PrintArray(arr, "Случайный массив:");
+int[] sumInLines = SumEachLine(arr);
+int index = MinNumberIndex(sumInLines);
+Console.WriteLine($"Наименьшая сумма элементов в строке {index}");
