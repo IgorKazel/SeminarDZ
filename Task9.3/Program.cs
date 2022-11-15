f521c6ad8c973daaf1dae684c3ba22ad4a30a2f6
Igor Kazel <igor.joer@gmail.com>
2022-11-15 21:22:40 +0300

    Task9.3 added

---
+++ Task9.3/Program.cs
@@ -0,0 +1,38 @@
+﻿int NumberEntry(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+bool CheckNumbers(int m, int n)
+{
+    if (m < 0 || n < 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+int AkkermannFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return AkkermannFunction(m - 1, 1);
+    }
+    return AkkermannFunction(m - 1, AkkermannFunction(m, n - 1));
+}
+
+int m = NumberEntry("Введите натуральное число m:");
+int n = NumberEntry("Введите натуральное число n:");
+if (CheckNumbers(m, n))
+{
+    Console.WriteLine("Оба числа должны быть неотрицательными");
+    return;
+}
+int result = AkkermannFunction(m, n);
+Console.WriteLine($"Функция Аккермана: A({m},{n}) = {result}");
